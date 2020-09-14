4922e564033ead6555b6cc951d98d0923e604a1d
Marcos <teacher.mrdv@gmail.com>
2020-09-14 18:36:23 +0800

    more testing-remove

--- src/ZooTest.java
+++ src/ZooTest.java
@@ -2,26 +2,41 @@ import java.util.*;
 
 public class ZooTest
 {
-	public int findName(String name)
+	public static int findName(ArrayList<Animal> animalList, String name)
 	{
 		int index = -1;
-		
+		int len = animalList.size();
+		if( len > 0)
+		{	for(int i=0; i < len; i++)
+			{	if( animalList.get(i).getName().equalsIgnoreCase(name) )
+				{	index = i;
+					break;
+				}
+			}
+		}
 		return index;
 	}
 	
 	public static void main(String[] args)
 	{
-		ArrayList<Animal> alist = new ArrayList<Animal>();
-		Dog pet = new Dog("Bob", 2010, "Chihuahua");
-		alist.add(pet);
-		pet = new Dog("Can", 2009, "Loly");
-		alist.add(pet);
-		pet = new Dog("Bibi", 2012, "Lisa");
-		alist.add(pet);
-		Sheep baah = new Sheep("Beba", 2009, false);
-		alist.add(baah);
-		Zoo loco = new Zoo("Loco", alist);
-		System.out.println(loco);
+		ArrayList<Animal> animals = new ArrayList<Animal>();
+		Dog doggie = new Dog("Alberto", 2010, "Chihuahua");
+		animals.add(doggie);
+		doggie = new Dog("Snoop", 2009, "Rapper");
+		animals.add(doggie);
+		doggie = new Dog("Bibi", 2012, "Terrier");
+		animals.add(doggie);
+		Sheep fluffy = new Sheep("Shaun", 2009, false);
+		animals.add(fluffy);
+		Zoo myZoo = new Zoo("Loco", animals);
+		System.out.println(myZoo);
+		System.out.println();
+		String name = "bibi";
+		int indexToRemove = findName(animals, name);
+		System.out.println( indexToRemove );
+		animals.remove(indexToRemove); // delete item
+		System.out.println(myZoo);
+				
 		
 	}
 
